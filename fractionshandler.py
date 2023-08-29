5648a9792da61c6ae2bdfd45e7283686a351b8cd
Reyralona <reypetrucci1@gmail.com>
2023-08-29 17:57:04 -0300

    added return values

--- fractionshandler.py
+++ fractionshandler.py
@@ -2,18 +2,9 @@ from math import lcm, gcd
 from operator import sub, mul
 from functools import reduce
 
-def print_mixed_fraction(mi, rem, den):
-    if (mi == 0):
-        print_fraction(rem, den)
-    else:
-        print(f'{mi} ({rem} / {den})')
-
-def print_fraction(num, den):
-    print(f'{num} / {den}')
-
 def simplify_fraction(num, den):
     if (num % den == 0):
-        print_fraction(num, den)
+        return [num, den]
     else:
         g = gcd(abs(num), abs(den))
         num = num // g
@@ -25,11 +16,15 @@ def simplify_fraction(num, den):
             else:
                 rem = abs(num) % abs(den)
                 mi = abs(num) // abs(den)
-            print_mixed_fraction(mi, rem, den)
+            
+            if(mi == 0):
+                return[rem, den]
+            else:
+                return[mi, rem, den]
         elif (num % den != 0):
             mi = num // den
             rem = num % den
-            print_mixed_fraction(mi, rem, den)
+            return[mi, rem, den]
 
 def equal_denominators(*fractions):
     denominators = list(i[1] for i in list(*fractions))
@@ -44,26 +39,26 @@ def mult_fractions(*fractions):
 
 def fraction_sum(*fractions):
     if (equal_denominators(fractions)):
-        simplify_fraction(
-            (sum(list(i[0] for i in fractions))), fractions[0][1])
+        return simplify_fraction((sum(list(i[0] for i in fractions))), fractions[0][1])
+        
     else:
         newden = lcm(*list(i[1] for i in fractions))
-        fraction_sum(
-            *list(mult_fractions([i[0], i[1]], [newden//i[1], newden//i[1]]) for i in fractions))
+        return fraction_sum(*list(mult_fractions([i[0], i[1]], [newden//i[1], newden//i[1]]) for i in fractions))
 
 def fraction_sub(*fractions):
     if (equal_denominators(fractions)):
-        simplify_fraction(
-            reduce(sub, (list([i[0] for i in fractions]))), fractions[0][1])
+        return simplify_fraction(reduce(sub, (list([i[0] for i in fractions]))), fractions[0][1])
     else:
         newden = lcm(*list(i[1] for i in fractions))
-        fraction_sub(
-            *list(mult_fractions([i[0], i[1]], [newden//i[1], newden//i[1]]) for i in fractions))
+        return fraction_sub(*list(mult_fractions([i[0], i[1]], [newden//i[1], newden//i[1]]) for i in fractions))
 
 def fraction_mul(*fractions):
-    simplify_fraction(*mult_fractions(*fractions))
+    return simplify_fraction(*mult_fractions(*fractions))
 
 def fraction_div(*fractions):
     first = fractions[0]
     newfractions = invert_fractions(*fractions[1:])
-    fraction_mul(first, *newfractions)
+    return fraction_mul(first, *newfractions)
+
+exp = fraction_sum(fraction_sum([2,3], [3,5]), fraction_sum([1,3], [3, 5]))
+print(exp)
